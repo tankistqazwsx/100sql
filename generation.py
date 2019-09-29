a8ae1d2a1e838bb60df55f4de117d7dbe61de109
Sapfir0 <sapfir999999@yandex.ru>
2019-09-29 20:06:54 +0400

    почти что код стайл фиксы

--- generation.py
+++ generation.py
@@ -1,6 +1,5 @@
 import enum
 import random
-import datetime
 
 from bunch import generateRandomBunch
 
@@ -8,14 +7,21 @@ from tables import tables
 
 insertUpdateData = ["Александроус", "Андреев", "Леха", "Колян"]
 
-    
-def generateColumns(tableName : str) -> str:
+
+def generateColumns(tableName: str) -> str:
     table_keys = list(tables[tableName].keys())
     bunch_of_keys = generateRandomBunch(table_keys)
     columns_str = ""
     for i in range(len(bunch_of_keys)):
-        columns_str += '\'' + bunch_of_keys[i] + '\'' + ('' if i == len(bunch_of_keys)-1 else ',')
+        columns_str += singleQuote(bunch_of_keys[i])
+        if i != len(bunch_of_keys) - 1:
+            columns_str += ','
+
     return columns_str
+
+
+def singleQuote(word):
+    return f"'{word}'"
 
 
 def generateWhereStatements():
@@ -26,6 +32,7 @@ def generateWhereStatements():
                 columnType = table[column]["type"]
                 if columnType == "VARCHAR":
                     whereElements = f"{random.choice(insertUpdateData)}"
+                    whereElements = singleQuote(whereElements)
                 elif columnType == "INTEGER":
                     whereElements = random.randint(0, 1000)
                 elif columnType == "FLOAT":
@@ -38,12 +45,11 @@ def generateWhereStatements():
 
 def abstractComand(command, table='', selectStatement='') -> str:
     stringCommand = random.choice(command.value)
-    
+
     if not table:
         table = random.choice(list(tables.keys()))
         # пока допустим что только один оператор
-        operationsColumn = random.choice(list(tables[table].values()))
-    
+
     where = generateWhereStatements()
 
     if command == sql.DELETE:
@@ -71,10 +77,10 @@ class sql(enum.Enum):
     UPDATE = "UPDATE",
     DELETE = "DELETE",
     INSERT = "INSERT",
-    ZATICHKA = ""
+
 
 def generateCreatingTable(table_name):
-    res = f"CREATE TABLE {table_name} ("
+    res = f"CREATE TABLE IF NOT EXISTS {table_name} ("
     table = tables[table_name]
     for column_name in table:
         column_type = table[column_name]["type"]
@@ -86,21 +92,22 @@ def generateCreatingTable(table_name):
         name_and_type = f"{column_name} {column_type}"
         res += name_and_type + ", "
     res += ");"
+
     return res
 
+
 def generateSelect(table_name):
-    table = random.choice(list(tables.keys()))
-    selectStatement=''
-    columns = generateColumns(table)
+    selectStatement = ''
+    columns = generateColumns(table_name)
     where = generateWhereStatements()
-    return f"SELECT {columns} from {table}{selectStatement} WHERE {where};"
+    return f"SELECT {columns} from {table_name}{selectStatement} WHERE {where};"
+
 
 def generate_sql_requests():
     for table_name in tables:
         yield generateCreatingTable(table_name)
 
-
     for i in range(50):
         funarray = [updateTemplate(), generateSelect(random.choice(list(tables.keys()))),
-                deleteTemplate(), insertTemplate()]
+                    deleteTemplate(), insertTemplate()]
         yield random.choice(funarray)
