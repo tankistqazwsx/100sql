ff733f9908803bbc886145b29d8cd3f58f6334b5
kyplyk <avdosev1@yandex.ru>
2019-09-29 19:06:08 +0400

    отделил вывод и генерацию

---
+++ generation.py
@@ -0,0 +1,106 @@
+import enum
+import random
+import datetime
+
+from bunch import generateRandomBunch
+
+from tables import tables
+
+insertUpdateData = ["Александроус", "Андреев", "Леха", "Колян"]
+
+    
+def generateColumns(tableName : str) -> str:
+    table_keys = list(tables[tableName].keys())
+    bunch_of_keys = generateRandomBunch(table_keys)
+    columns_str = ""
+    for i in range(len(bunch_of_keys)):
+        columns_str += '\'' + bunch_of_keys[i] + '\'' + ('' if i == len(bunch_of_keys)-1 else ',')
+    return columns_str
+
+
+def generateWhereStatements():
+    whereArrays = []
+    for table in tables.values():
+        for column in table:
+            if column != "*":
+                columnType = table[column]["type"]
+                if columnType == "VARCHAR":
+                    whereElements = f"{random.choice(insertUpdateData)}"
+                elif columnType == "INTEGER":
+                    whereElements = random.randint(0, 1000)
+                elif columnType == "FLOAT":
+                    whereElements = random.random()
+                else:
+                    whereElements = None
+                whereArrays.append(f"{column}={whereElements}")
+    return random.choice(whereArrays)
+
+
+def abstractComand(command, table='', selectStatement='') -> str:
+    stringCommand = random.choice(command.value)
+    
+    if not table:
+        table = random.choice(list(tables.keys()))
+        # пока допустим что только один оператор
+        operationsColumn = random.choice(list(tables[table].values()))
+    
+    where = generateWhereStatements()
+
+    if command == sql.DELETE:
+        return f"{stringCommand} from {table}{selectStatement} WHERE {where};"
+    elif command == sql.INSERT:
+        return f"{stringCommand} INTO {table} VALUES ({where});"
+    elif command == sql.UPDATE:
+        return f"{stringCommand} {table} SET {{ }};"
+
+
+def updateTemplate():
+    return abstractComand(sql.UPDATE)
+
+
+def deleteTemplate():
+    return abstractComand(sql.DELETE)
+
+
+def insertTemplate():
+    return abstractComand(sql.INSERT)
+
+
+class sql(enum.Enum):
+    SELECT = "SELECT",
+    UPDATE = "UPDATE",
+    DELETE = "DELETE",
+    INSERT = "INSERT",
+    ZATICHKA = ""
+
+def generateCreatingTable(table_name):
+    res = f"CREATE TABLE {table_name} ("
+    table = tables[table_name]
+    for column_name in table:
+        column_type = table[column_name]["type"]
+        try:
+            column_type += "(" + table[column_name]["len"] + ")"
+        except KeyError as identifier:
+            pass
+        # <col_name1> <col_type1>
+        name_and_type = f"{column_name} {column_type}"
+        res += name_and_type + ", "
+    res += ");"
+    return res
+
+def generateSelect(table_name):
+    table = random.choice(list(tables.keys()))
+    selectStatement=''
+    columns = generateColumns(table)
+    where = generateWhereStatements()
+    return f"SELECT {columns} from {table}{selectStatement} WHERE {where};"
+
+def generate_sql_requests():
+    for table_name in tables:
+        yield generateCreatingTable(table_name)
+
+
+    for i in range(50):
+        funarray = [updateTemplate(), generateSelect(random.choice(list(tables.keys()))),
+                deleteTemplate(), insertTemplate()]
+        yield random.choice(funarray)
