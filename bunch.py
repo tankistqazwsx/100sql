081f8e999120e9254d6276588f70d8ea78357e23
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2019-10-02 16:44:04 +0300

    update

--- bunch.py
+++ bunch.py
@@ -1,5 +1,6 @@
 import random
 
+
 def generation(arr: list, dlina: int) -> list:
 	def generator(sgen: list, prev_index: int = -1):
 		if len(sgen) < dlina:
@@ -11,7 +12,8 @@ def generation(arr: list, dlina: int) -> list:
 			yield sgen.copy()
 	return [i for i in generator([])]
 
+
 def generateRandomBunch(arr: list):
-    bunch = random.choice(generation(arr, random.randrange(1, len(arr))))
-    random.shuffle(bunch)
-    return bunch
+	bunch = random.choice(generation(arr, random.randrange(1, len(arr))))
+	random.shuffle(bunch)
+	return bunch
